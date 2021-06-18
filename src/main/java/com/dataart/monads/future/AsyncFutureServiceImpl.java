c9738102400a2b17a207351ad09732739bf48e17
klirola <khose.lirola@dataart.com>
2021-06-18 16:39:30 +0300

    prepared AsyncFutureServiceImpl implementation

--- src/main/java/com/dataart/monads/future/AsyncFutureServiceImpl.java
+++ src/main/java/com/dataart/monads/future/AsyncFutureServiceImpl.java
@@ -7,21 +7,27 @@ import com.dataart.core.data.Worker;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 public class AsyncFutureServiceImpl {
 
 
     public static CompletableFuture<Integer> handleString(CompletableFuture<String> future) {
-        return null;
+        return future.thenApply(s -> Integer.parseInt(s) * 2);
+
     }
 
     public static Integer handleException(CompletableFuture<String> future) throws ExecutionException, InterruptedException {
-        return null;
+        return future
+                .exceptionally(err -> String.valueOf(-1))
+                .thenApply(Integer::parseInt).join();
+
     }
 
-    public static CompletableFuture<List<Worker>> handleFutures(CompletableFuture<Company> companyCompletableFuture, CompletableFuture<Profession> professionCompletableFuture) throws ExecutionException, InterruptedException {
-        return null;
+    public static CompletableFuture<List<Worker>> handleFutures(CompletableFuture<Company> companyCompletableFuture,
+                                                                CompletableFuture<Profession> professionCompletableFuture) throws ExecutionException, InterruptedException {
+
+        return companyCompletableFuture.thenApply(company -> company
+                .getWorkers().orElse(null));
     }
 
 }
