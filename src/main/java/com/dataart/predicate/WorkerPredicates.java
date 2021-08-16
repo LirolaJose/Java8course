7edfbb57e04398751fcaee8168beea708f09276f
Khose Lirola <khose.lirola@dataart.com>
2021-08-16 11:07:09 +0300

    use TIP's

--- src/main/java/com/dataart/predicate/WorkerPredicates.java
+++ src/main/java/com/dataart/predicate/WorkerPredicates.java
@@ -8,10 +8,12 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class WorkerPredicates {
+    public static final int ADULT_AGE = 20;
 
     // TIPS: you can use isAgeMoreThan method
     public static Predicate<Worker> isAdult() {
-        return worker -> worker.getAge() > 20;
+        return isAgeMoreThan(ADULT_AGE);
+//        return worker -> worker.getAge() > 20;
     }
 
     public static Predicate<Worker> isAgeMoreThan(int age) {
@@ -20,12 +22,13 @@ public class WorkerPredicates {
 
     // TIPS: you can use isAgeMoreThan method
     public static Predicate<Worker> isAgeMoreThanAndProfession(int age, Profession profession) {
-        return worker -> worker.getAge() > age && worker.getProfession().equals(profession);
+//        return worker -> worker.getAge() > age && worker.getProfession().equals(profession);
+        return isAgeMoreThan(age).and(worker -> worker.getProfession().equals(profession));
     }
 
     // TIPS: you can use isAgeMoreThan method
     public static Predicate<Worker> isAgeMoreThanOrProfession(int age, Profession profession) {
-        return worker -> worker.getAge() > age || worker.getProfession().equals(profession);
+        return isAgeMoreThan(age).or(worker -> worker.getProfession().equals(profession));
     }
 
     public static List<Worker> filterWorkers(List<Worker> workerList, Predicate<Worker> predicate) {
